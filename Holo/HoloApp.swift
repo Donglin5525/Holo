bcdb4c4c275d2f341920b32576c1eaa77b9a05fe
Tang YuXuan <tangyuxuan1994@gmail.com>
2026-03-01 17:04:32 +0800

    Initial Commit

---
+++ Holo/HoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoloApp.swift
+//  Holo
+//
+//  Created by 唐予轩 on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct HoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
