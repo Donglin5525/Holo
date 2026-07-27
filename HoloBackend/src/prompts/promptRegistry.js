9312399a85931e51a9210ae2e2e6fe02c0f60054
Tang YuXuan <tangyuxuan1994@gmail.com>
2026-07-28 01:25:26 +0800

    chore(backend): deploy HoloBackend updates

--- HoloBackend/src/prompts/promptRegistry.js
+++ HoloBackend/src/prompts/promptRegistry.js
@@ -18,7 +18,7 @@ const PROMPT_VERSIONS = {
   health_insight_generation: 2,
   thought_organization: 3,
   thought_tag_convergence: 2,
-  agent_loop: 15,
+  agent_loop: 16,
   memory_domain_extraction: 2,
   memory_cross_domain_fusion: 2,
 };
@@ -30,6 +30,7 @@ const PROMPT_CONTRACT_APPENDICES = {
     defaultPrompts._agent_loop_v11_contract,
     defaultPrompts._agent_loop_v14_contract,
     defaultPrompts._agent_loop_v15_contract,
+    defaultPrompts._agent_loop_v16_contract,
   ],
   memory_insight_generation: [defaultPrompts._memory_semantic_v2_contract],
   memory_domain_extraction: [defaultPrompts._memory_domain_quality_v2_contract],
